bfd409e663467a6c5efaf7c088e3bd562587ac2b
Edoardo Vacchi <evacchi@users.noreply.github.com>
2023-03-18 18:50:38 +0100

    terminal-windows

--- internal/platform/terminal_windows.go
+++ internal/platform/terminal_windows.go
@@ -8,7 +8,19 @@ import (
 var procGetConsoleMode = kernel32.NewProc("GetConsoleMode")
 
 func isTerminal(fd uintptr) bool {
+	var h syscall.Handle
+	switch fd {
+	case 0:
+		h = syscall.Stdin
+	case 1:
+		h = syscall.Stdout
+	case 3:
+		h = syscall.Stderr
+	default:
+		h = syscall.Handle(fd)
+	}
+
 	var st uint32
-	r, _, e := syscall.Syscall(procGetConsoleMode.Addr(), 2, uintptr(fd), uintptr(unsafe.Pointer(&st)), 0)
+	r, _, e := syscall.Syscall(procGetConsoleMode.Addr(), 2, uintptr(h), uintptr(unsafe.Pointer(&st)), 0)
 	return r != 0 && e == 0
 }
