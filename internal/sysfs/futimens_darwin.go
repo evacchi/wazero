b794c3b229aa3974a2edf5fd0321f6fea5964b09
Edoardo Vacchi <evacchi@users.noreply.github.com>
2023-07-07 14:57:46 +0200

    wip
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

--- internal/sysfs/futimens_darwin.go
+++ internal/sysfs/futimens_darwin.go
@@ -2,6 +2,7 @@ package sysfs
 
 import (
 	"syscall"
+	"unsafe"
 	_ "unsafe" // for go:linkname
 )
 
@@ -25,15 +26,76 @@ func utimens(path string, times *[2]syscall.Timespec, symlinkFollow bool) error 
 	return utimensat(_AT_FDCWD, path, times, flags)
 }
 
-func futimens(fd uintptr, times *[2]syscall.Timespec) error {
-	_p0 := timesToPtr(times)
-
-	// Warning: futimens only exists since High Sierra (10.13).
-	_, _, e1 := syscall_syscall6(libc_futimens_trampoline_addr, fd, uintptr(_p0), 0, 0, 0, 0)
-	if e1 != 0 {
-		return e1
+func prepareTimesAndAttrs(ts *[2]syscall.Timespec) (attrs, size int, times [2]syscall.Timespec) {
+	const sizeOfTimespec = int(unsafe.Sizeof(times[0]))
+	i := 0
+	if ts[1].Nsec != _UTIME_OMIT {
+		attrs |= _ATTR_CMN_MODTIME
+		times[i] = ts[1]
+		i++
 	}
-	return nil
+	if ts[0].Nsec != _UTIME_OMIT {
+		attrs |= _ATTR_CMN_ACCTIME
+		times[i] = ts[0]
+		i++
+	}
+	return attrs, i * sizeOfTimespec, times
+}
+
+type attrlist struct {
+	bitmapcount byte   /* number of attr. bit sets in list (should be 5) */
+	reserved    uint16 /* (to maintain 4-byte alignment) */
+	commonattr  uint32 /* common attribute group */
+	volattr     uint32 /* Volume attribute group */
+	dirattr     uint32 /* directory attribute group */
+	fileattr    uint32 /* file attribute group */
+	forkattr    uint32 /* fork attribute group */
+}
+
+const _ATTR_BIT_MAP_COUNT = 5
+const _ATTR_CMN_MODTIME = 0x00000400
+const _ATTR_CMN_ACCTIME = 0x00001000
+
+func futimens(fd uintptr, times *[2]syscall.Timespec) error {
+	//if times == nil {
+	//	times = &[2]syscall.Timespec{
+	//		{
+	//			Sec: _UTIME_NOW, Nsec: _UTIME_NOW,
+	//		},
+	//		{
+	//			Sec: _UTIME_NOW, Nsec: _UTIME_NOW,
+	//		},
+	//	}
+	//}
+	//_p0 := timesToPtr(times)
+	//
+	//// Warning: futimens only exists since High Sierra (10.13).
+	//e1 := fsetattrlist(fd, uintptr(_p0), 0, 0, 0, 0)
+	//if e1 != 0 {
+	//	return e1
+	//}
+	//return nil
+
+	attrs, size, ts := prepareTimesAndAttrs(times)
+	attrlist := attrlist{
+		bitmapcount: _ATTR_BIT_MAP_COUNT,
+		commonattr:  uint32(attrs),
+	}
+	return fsetattrlist(fd, &attrlist, unsafe.Pointer(&ts), size, 0)
+
+}
+
+func fsetattrlist(fd uintptr, attrlist *attrlist, attrbuf unsafe.Pointer, attrbufsize int, options uint32) syscall.Errno {
+	_, _, errno := syscall.Syscall6(
+		uintptr(syscall.SYS_FSETATTRLIST),
+		fd,
+		uintptr(unsafe.Pointer(attrlist)),
+		uintptr(attrbuf),
+		uintptr(attrbufsize),
+		uintptr(options),
+		uintptr(0),
+	)
+	return errno
 }
 
 // libc_futimens_trampoline_addr is the address of the
